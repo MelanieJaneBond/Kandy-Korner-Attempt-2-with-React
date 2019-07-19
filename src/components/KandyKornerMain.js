dd578f6a8a0ab9d67e5bc30db3b9d697b217755e
MelanieJaneBond <miss.bond2@gmail.com>
2019-07-19 14:16:03 -0500

    added a modules directory

--- src/components/KandyKornerMain.js
+++ src/components/KandyKornerMain.js
@@ -16,3 +16,17 @@ class Master extends Component {
 }
 
 export default Master
+
+//Add a function in KandyKorner that will remove individual candies from your inventory.
+//Add a button to the JSX you have in CandyList that renders individual candies.
+//Then invoke that function when the user clicks the button on an individual candy.
+
+//NEXT chapter's assignment:
+//Create a manager file in the modules directory for each of your resources.
+
+// Candies
+// Locations
+// Employees
+// Candy Types
+// Add the get() and all() methods to each one, changing the URL path in each one to get the corresponding resource type.
+//Then refactor the ApplicationViews component to import all of them and use all of them when querying your data.
